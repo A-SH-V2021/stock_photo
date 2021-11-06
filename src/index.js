39e8536688861ac832f3713ca8d0c04a297b67ce
ali.sh <narenje20142014@gmail.com>
2021-11-06 20:41:42 +0330

    start project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { AppProvider } from "./context/Context";
+import App from "./App";
+
+ReactDOM.render(
+  <AppProvider>
+    <App />
+  </AppProvider>,
+  document.getElementById("root")
+);
